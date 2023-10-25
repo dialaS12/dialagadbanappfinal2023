d11fb08c5b17613fbadd4c3a20f7b0e3bdbdbfc7
diala <dialagadban5@gmail.com>
2023-10-25 11:35:32 +0300

    Merge remote-tracking branch 'origin/master' اكمال الشاشات

---
+++ app/src/main/java/dga/example/dialagadbanappfinal2023/signIn1.java
@@ -0,0 +1,14 @@
+package dga.example.dialagadbanappfinal2023;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class signIn1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in1);
+    }
+}
--- app/src/main/java/dga/example/dialagadbanappfinal2023/singUp1.java
+++ app/src/main/java/dga/example/dialagadbanappfinal2023/singUp1.java
@@ -3,12 +3,29 @@ package dga.example.dialagadbanappfinal2023;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
-public class singUp1 extends AppCompatActivity {
+import com.google.android.material.textfield.TextInputEditText;
+
+public class singUp1 extends AppCompatActivity
+{
+    private TextInputEditText etEmail;
+    private TextInputEditText etpassword;
+    private TextInputEditText etRePassword;
+    private TextInputEditText etName;
+    private TextInputEditText etPhone;
+    private Button btnSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sing_up1);
+        setContentView(R.layout.activity_sing_up1);
+        etEmail= (TextInputEditText) findViewById(R.id.etPassword);
+        etpassword= (TextInputEditText) findViewById(R.id.etPassword);
+        etRePassword= (TextInputEditText) findViewById(R.id.etRePassword);
+        etName= (TextInputEditText) findViewById(R.id.etName);
+        etPhone= (TextInputEditText) findViewById(R.id.etPhone);
+        btnSave= (Button) findViewById(R.id.btnSave);
     }
 }
