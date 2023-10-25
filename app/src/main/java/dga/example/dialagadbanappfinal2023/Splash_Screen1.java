3dd5089f158d7974dc592e8c7c747a67800bf4b7
dialaS12 <dialagadban5@gmail.com>
2023-10-25 22:21:43 +0300

    hg

---
+++ app/src/main/java/dga/example/dialagadbanappfinal2023/Splash_Screen1.java
@@ -0,0 +1,14 @@
+package dga.example.dialagadbanappfinal2023;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Splash_Screen1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen1);
+    }
+}
--- app/src/main/java/dga/example/dialagadbanappfinal2023/signIn1.java
+++ app/src/main/java/dga/example/dialagadbanappfinal2023/signIn1.java
@@ -3,12 +3,26 @@ package dga.example.dialagadbanappfinal2023;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
-public class signIn1 extends AppCompatActivity {
+import com.google.android.material.textfield.TextInputEditText;
+
+public class signIn1 extends AppCompatActivity
+{
+    private TextInputEditText etEmail;
+    private TextInputEditText etpassword;
+    private Button btnSingin;
+    private Button btnSingup;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in1);
+        etEmail= (TextInputEditText) findViewById(R.id.etEmail);
+        etpassword= (TextInputEditText) findViewById(R.id.etPassword);
+        btnSingin= (Button) findViewById(R.id.btnSingin);
+        btnSingup= (Button) findViewById(R.id.btnSingup);
     }
 }
+
