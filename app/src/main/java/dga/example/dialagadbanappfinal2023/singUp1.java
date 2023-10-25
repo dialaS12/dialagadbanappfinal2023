8786adb8cd125c99739ada95277ba8af9c556775
diala <dialagadban5@gmail.com>
2023-10-25 08:53:24 +0300

    hv

---
+++ app/src/main/java/dga/example/dialagadbanappfinal2023/singUp1.java
@@ -0,0 +1,14 @@
+package dga.example.dialagadbanappfinal2023;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class singUp1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sing_up1);
+    }
+}
