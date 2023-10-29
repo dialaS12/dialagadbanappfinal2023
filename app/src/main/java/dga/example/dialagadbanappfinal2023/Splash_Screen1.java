1527e9f09ab21dcfc6904aee3e33c56d9c43ab06
diala <dialagadban5@gmail.com>
2023-10-29 12:26:13 +0200

    Merge remote-tracking branch 'origin/master' اكمال الشاشات

--- app/src/main/java/dga/example/dialagadbanappfinal2023/Splash_Screen1.java
+++ app/src/main/java/dga/example/dialagadbanappfinal2023/Splash_Screen1.java
@@ -2,7 +2,9 @@ package dga.example.dialagadbanappfinal2023;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Splash_Screen1 extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class Splash_Screen1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen1);
+        Handler h=new Handler();
+        Runnable r=new Runnable() {
+            @Override
+            public void run()
+            {
+                //to open new activity from current to next
+                Intent i= new Intent(Splash_Screen1.this, signIn1.class);
+                startActivity(i);
+                //to close current activity
+                finish();
+            }
+        };
+        h.postDelayed(r,3000);
     }
+
 }
