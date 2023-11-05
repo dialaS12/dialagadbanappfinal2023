24ab68244484b87b073a9306890addcb4b7e4f1b
diala <dialagadban5@gmail.com>
2023-11-05 12:23:01 +0200

    Merge remote-tracking branch 'origin/master' اكمال الشاشات

--- app/src/main/java/dga/example/dialagadbanappfinal2023/signIn1.java
+++ app/src/main/java/dga/example/dialagadbanappfinal2023/signIn1.java
@@ -2,13 +2,14 @@ package dga.example.dialagadbanappfinal2023;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.google.android.material.textfield.TextInputEditText;
 
-public class signIn1 extends AppCompatActivity
-{
+public class signIn1 extends AppCompatActivity {
     private TextInputEditText etEmail;
     private TextInputEditText etpassword;
     private Button btnSingin;
@@ -19,10 +20,20 @@ public class signIn1 extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in1);
-        etEmail= (TextInputEditText) findViewById(R.id.etEmail);
-        etpassword= (TextInputEditText) findViewById(R.id.etPassword);
-        btnSingin= (Button) findViewById(R.id.btnSingin);
-        btnSingup= (Button) findViewById(R.id.btnSingup);
+        etEmail = (TextInputEditText) findViewById(R.id.etEmail);
+        etpassword = (TextInputEditText) findViewById(R.id.etPassword);
+        btnSingin = (Button) findViewById(R.id.btnSingin);
+        btnSingup = (Button) findViewById(R.id.btnSingup);
+    }
+    public void onClickSingUp(View v)
+    {
+        //to open new activity from current to next
+        Intent i= new Intent(signIn1.this, singUp1.class);
+        startActivity(i);
+        //to close current activity
+        finish();
     }
 }
+
+
 
