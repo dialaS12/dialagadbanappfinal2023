5e5bfabae25191950789a362e84e72f1d70220bf
diala <dialagadban5@gmail.com>
2024-02-06 23:28:17 +0200

    تكميل الشاشات

--- app/src/main/java/dga/example/dialagadbanappfinal2023/singUp1.java
+++ app/src/main/java/dga/example/dialagadbanappfinal2023/singUp1.java
@@ -1,5 +1,6 @@
 package dga.example.dialagadbanappfinal2023;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -106,4 +107,55 @@ public class singUp1 extends AppCompatActivity {
 
         }
     }
+
+    public void onClickSave1 (View v)
+
+    {
+        checkEPRP_FB();
+    }
+
+    public void checkEPRP_FB()
+    {
+        boolean isAllOk1 = true;
+        String name1 = etName.getText().toString();
+        String email1 = etEmail.getText().toString();
+        String password1 = etpassword.getText().toString();
+        String repassword1 = etRePassword.getText().toString();
+        String phone1 = etPhone.getText().toString();
+
+
+        if (email1.length() < 6 || email1.contains("@") == false) {
+            isAllOk1 = false;
+            etEmail.setError("Wrong Email");
+        }
+        if (password1.length() < 8 || password1.contains(" ") == true) {
+            isAllOk1 = false;
+            etpassword.setError("Wrong Password");
+        }
+
+        if (repassword1.length() != password1.length() || repassword1.equals(password1) == false) {
+            isAllOk1 = false;
+            etRePassword.setError("Wrong RePassword");
+        }
+
+        if (phone1.length() < 10 || phone1.length() > 10 || phone1.length() != 10 || phone1.contains(" ") == true) {
+            isAllOk1 = false;
+            etPhone.setError("Wrong Phone");
+        }
+        if (isAllOk1) {
+            //كائن لعمليه التسجيل
+            Firebas auth= FirebaseAuth.getInstance();
+            //צירת חשבון בעזרת מיל וסיסמה
+
+
+
+
+        }
+
+
+    }
+
+
+}
+
 }
